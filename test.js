5b9ac5c6094abd137239b0445bc5516bfbd7f8db
huangyuan <xiaoyuan98@126.com>
2024-03-26 11:13:22 +0800

    调整

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
-const fs = require("fs");
-const fileList = fs.readdirSync("./needPostImg");
-console.log(fileList)
+let newName = "ANQUANRENZHENG()@2X"
+newName = newName.replace(/\s+/g, "");
+    newName = newName.replace('@2X', "");
+    newName = newName.replace('@1X', "");
+    newName = newName.replace(/\([0-9]*\)/, "");
+
+console.log(newName)
