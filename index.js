749292a41641c221c56b66d17ce6f955925a237e
huangyuan <xiaoyuan98@126.com>
2024-03-30 16:38:33 +0800

    优化

--- index.js
+++ index.js
@@ -5,92 +5,104 @@ const pinyin = require("pinyin");
 
 
 const token =
-"";
+    "";
 const IMG_SERVICE_URL = 'http://test.api.yichuyun.cn/api/obs/upload'
 
 // 上传后的所有文件信息
 let filePostedArr = [];
-let filePostedObj ={}
+let filePostedObj = {}
 
 const supportFileType = ['png', 'jpg']
 
 async function uploadFile({
-  file,
-  save_path = "imgs",
-  file_key = "upload",
-  file_pre = "img",
-  token,
+    file,
+    save_path = "imgs",
+    file_key = "upload",
+    file_pre = "img",
+    token,
 }) {
-  let formData = new FormData();
-  formData.append("upload", file);
-  formData.append("file_key", file_key);
-  formData.append("save_path", save_path);
-  formData.append("file_pre", file_pre);
-  let len = await new Promise((resolve, reject) => {
-    return formData.getLength((err, length) =>
-      err ? reject(err) : resolve(length)
-    );
-  });
-  const res = await axios({
-    url: IMG_SERVICE_URL,
-    method: "POST",
-    data: formData,
-    headers: {
-      "Content-Length": len,
-      "Content-Type": "multipart/form-data",
-      Authorization: token,
-    },
-  });
-  return res.data;
+    let formData = new FormData();
+    formData.append("upload", file);
+    formData.append("file_key", file_key);
+    formData.append("save_path", save_path);
+    formData.append("file_pre", file_pre);
+    let len = await new Promise((resolve, reject) => {
+        return formData.getLength((err, length) =>
+            err ? reject(err) : resolve(length)
+        );
+    });
+    const res = await axios({
+        url: IMG_SERVICE_URL,
+        method: "POST",
+        data: formData,
+        headers: {
+            "Content-Length": len,
+            "Content-Type": "multipart/form-data",
+            Authorization: token,
+        },
+    });
+    return res.data;
 }
 async function writFile({
-  fileName,
-  str
-}){
-  const res = await fs.writeFileSync('./'+fileName,str)
-  return res
+    fileName,
+    str
+}) {
+    const res = await fs.writeFileSync('./' + fileName, str)
+    return res
 }
 
 async function main() {
-  const fileList = fs.readdirSync("./needPostImg").filter(item => {
-    const fileType = item.split('.')[1]
-    return supportFileType.includes(fileType)
-  })
-  filePostedArr = Array.from(Array(fileList.length))
 
-  fileList.forEach(async (fileName,index) => {
-    let imgFile = fs.createReadStream("./needPostImg/" + fileName);
-    const res = await uploadFile({
-      file: imgFile,
-      token,
-    });
+    let hasUnsupportFile = false
+    const fileList = fs.readdirSync("./needPostImg").filter(item => {
+        const fileType = item.split('.')[1]
+        const isSupport = supportFileType.includes(fileType)
+        if (hasUnsupportFile) {
+            hasUnsupportFile = true
+        }
+        return isSupport
+    })
 
-    const originFileName = fileName.split(".")[0];
-    let newName = pinyin(originFileName, {
-      style: pinyin.STYLE_NORMAL,
-      heteronym: false
-    }).join("");
-    newName = newName.replace(/\s+/g, "");
-    newName = newName.replace('@2X', "");
-    newName = newName.replace('@1X', "");
-    newName = newName.replace(/\([0-9a-z]*\)/gi, "");
-
-    const info = {
-      fileName: newName.replace(/\s+/g, ""),
-      url: res.data.upload,
-    };
-    filePostedArr[index] = info
-
-    if (!filePostedArr.includes(undefined)){
-      // 生成对象结构
-      filePostedArr.forEach(item => {
-        filePostedObj[item.fileName.toUpperCase()] = item.url
-      })
-      // 写文件
-      await writFile({fileName:'./files.json',str: JSON.stringify(filePostedObj)})
-      console.log('生成完毕')
+    if (hasUnsupportFile) {
+        console.log('存在不支持的文件类型')
+        return
     }
-  });
+
+    filePostedArr = Array.from(Array(fileList.length))
+
+    fileList.forEach(async (fileName, index) => {
+        let imgFile = fs.createReadStream("./needPostImg/" + fileName);
+        const res = await uploadFile({
+            file: imgFile,
+            token,
+        });
+
+        console.log(res)
+
+        const originFileName = fileName.split(".")[0];
+        let newName = pinyin(originFileName, {
+            style: pinyin.STYLE_NORMAL,
+            heteronym: false
+        }).join("");
+        newName = newName.replace(/\s+/g, "");
+        newName = newName.replace(/(\@2x|\@1x)/gi, "");
+        newName = newName.replace(/\([0-9a-z]*\)/gi, "");
+        const info = {
+            fileName: newName,
+            url: res.data.upload,
+        };
+        filePostedArr[index] = info
+
+        if (!filePostedArr.includes(undefined)) {
+            // 生成对象结构
+            filePostedArr.forEach(item => {
+                filePostedObj[item.fileName.toUpperCase()] = item.url
+            })
+            // 写文件
+            await writFile({ fileName: './files.json', str: JSON.stringify(filePostedObj) })
+            console.log('生成完毕')
+        }
+    });
 
 }
 
--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-let newName = "ANQUANRENZHENG(Saa11)@2X"
+let newName = "tongzhi@2x"
 newName = newName.replace(/\s+/g, "");
-    newName = newName.replace('@2X', "");
-    newName = newName.replace('@1X', "");
+    newName = newName.replace(/(\@2x|\@1x)/gi, "");
     newName = newName.replace(/\([0-9a-z]*\)/gi, "");
 
 console.log(newName)
